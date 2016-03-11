dc39da9e157068d50368b658ee0eb7c8528c77c4
mocrowave3 <warrenericc@gmail.com>
2016-03-11 17:06:19 -0500

    Added game

---
+++ Road Trip/Assets/CarControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CarControl : MonoBehaviour 
+{
+
+	Rigidbody body;
+	float carSpeed;
+	bool isTouchingGround;
+
+	void Start() 
+	{
+		carSpeed = 5; 		//use number
+		body = GetComponent<Rigidbody>();
+		body.velocity += new Vector3(0, body.velocity.y, carSpeed);
+	} 
+
+	void Update() 
+	{
+		gameObject.transform.eulerAngles = new Vector3(0,0,0);
+		if(Input.GetKeyDown(KeyCode.A) == true) 								//key pressed
+			body.velocity = new Vector3(-1.5f, body.velocity.y, carSpeed);
+		if(Input.GetKeyDown(KeyCode.D) == true) 								//key pressed
+			body.velocity = new Vector3(1.5f, body.velocity.y, carSpeed);
+		if(Input.GetKey(KeyCode.D) == false && Input.GetKey(KeyCode.A) == false)
+			body.velocity = new Vector3(0, body.velocity.y ,carSpeed);
+		if(Input.GetKey(KeyCode.Space) && isTouchingGround == true) 
+			body.velocity = new Vector3(body.velocity.x,5,carSpeed);
+		isTouchingGround = false;
+	}
+
+	void OnCollisionStay(Collision Col)
+	{
+		if(Col.gameObject.tag == "ground")
+			isTouchingGround = true;
+	}
+}
