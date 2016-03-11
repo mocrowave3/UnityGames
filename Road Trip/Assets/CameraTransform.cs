0ae57240e283e0293d3d4fe5ce79b27f08f80461
mocrowave3 <warrenericc@gmail.com>
2016-03-11 17:33:03 -0500

    test1

---
+++ Road Trip/Assets/CameraTransform.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTransform : MonoBehaviour 
+{
+
+	bool pan;
+	int time;
+	bool runOnce;
+
+	void Start () 
+	{
+		runOnce = true;
+		pan = true;
+	}
+
+	void Update () 
+	{
+		if(pan) // camera panning
+		{
+			if(runOnce)
+			{
+				runOnce = false;
+				time = Time.time;
+			}
+			transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3(0, 0.75, 0.25), (Time.time - time)/2);
+			transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, Vector3(10, 0, 0), (Time.time - time)/2);
+		}
+	}
+}
